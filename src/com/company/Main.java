9e2099805983e6df4d99ba8e4e8a3538afdf684f
RomanKriukov <romank.official@gmail.com>
2021-04-21 22:28:26 +0300

    changed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello GOIT World!!!");
         System.out.println("This is our second commit");
+        System.out.println("This is our three commit");
+        System.out.println("This is our three commit");
     }
 }
